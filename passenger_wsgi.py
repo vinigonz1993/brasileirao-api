35b88d031957a8b38e437b2cb24fb33a49fe162a
vinigonz@terra.com.br <vinigonzalez1993@gmail.com>
2022-02-14 23:00:36 -0300

    update 2

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,19 +1,14 @@
-import os
-import sys
+"""
+Post request dont work on Djano apps on A2 hosted servers. This replacement 'passenger_wsgi.py'
+has fixed the issue for me. Works with Python 3.5 with Django 1.11, 2.0, 2.1, 2.2 and Python 3.7 with Django 3.0.
+"""
 
-import django.core.handlers.wsgi
-from django.core.wsgi import get_wsgi_application
-
-# Set up paths and environment variables
-sys.path.append(os.getcwd())
-os.environ['DJANGO_SETTINGS_MODULE'] = 'brasileiraoapi.settings'
-
-# Set script name for the PATH_INFO fix below
-SCRIPT_NAME = os.getcwd()
+# Keep this empty
+SCRIPT_NAME = ''
 
 class PassengerPathInfoFix(object):
     """
-        Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
+    Sets PATH_INFO from REQUEST_URI since Passenger doesn't provide it.
     """
     def __init__(self, app):
         self.app = app
@@ -21,12 +16,14 @@ class PassengerPathInfoFix(object):
     def __call__(self, environ, start_response):
         from urllib.parse import unquote
         environ['SCRIPT_NAME'] = SCRIPT_NAME
+
         request_uri = unquote(environ['REQUEST_URI'])
         script_name = unquote(environ.get('SCRIPT_NAME', ''))
         offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
         environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
         return self.app(environ, start_response)
 
-# Set the application
-application = get_wsgi_application()
+# Replace projectname with the name of the main folder containing wsgi.py and setting.py
+import brasileiraoapi.wsgi
+application = brasileiraoapi.wsgi.application
 application = PassengerPathInfoFix(application)
