99aaf14a0f963d5fecb3adc1c44308601dc055e9
vinigonz@terra.com.br <vinigonzalez1993@gmail.com>
2022-02-14 22:54:35 -0300

    update

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,39 +1,32 @@
 import os
 import sys
 
-#########Accessing the App's Default WSGI##############
-import brasileiraoapi.wsgi
+import django.core.handlers.wsgi
+from django.core.wsgi import get_wsgi_application
 
-application = brasileiraoapi.wsgi.application
+# Set up paths and environment variables
+sys.path.append(os.getcwd())
+os.environ['DJANGO_SETTINGS_MODULE'] = 'brasileiraoapi.settings'
 
-######## PASSENGER PATH INFO-FIX INITIALISATIONS#######
-cwd = os.getcwd()
-sys.path.append(cwd)
-#sys.path.append(os.getcwd())
-sys.path.append(cwd + '/brasileiraoapi')  #You must add your project here
-# Set this to your root
+# Set script name for the PATH_INFO fix below
 SCRIPT_NAME = os.getcwd()
 
-########  MIDDLEWARE CLASS TO FIX PASSENGER'S URI ISSUES #############
 class PassengerPathInfoFix(object):
     """
-    Sets PATH_INFO from REQUEST_URI since Passenger doesn't provide it.
+        Sets PATH_INFO from REQUEST_URI because Passenger doesn't provide it.
     """
     def __init__(self, app):
         self.app = app
 
     def __call__(self, environ, start_response):
-        #IF YOU ARE IN PYTHON 2.7 USE: from urllib import unquote
         from urllib.parse import unquote
         environ['SCRIPT_NAME'] = SCRIPT_NAME
-
         request_uri = unquote(environ['REQUEST_URI'])
         script_name = unquote(environ.get('SCRIPT_NAME', ''))
         offset = request_uri.startswith(script_name) and len(environ['SCRIPT_NAME']) or 0
         environ['PATH_INFO'] = request_uri[offset:].split('?', 1)[0]
         return self.app(environ, start_response)
 
-###########the redirecting Middleware
+# Set the application
+application = get_wsgi_application()
 application = PassengerPathInfoFix(application)
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
